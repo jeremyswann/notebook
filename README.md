398b38db2727a58e0a904f235439c1abd0578089
jeremyswann <mail@jeremyswann.com>
2018-07-25 13:13:03 +1000

    first commit :rocket:

---
+++ README.md
@@ -0,0 +1,30 @@
+# Notebook App :notebook:
+A **very** basic command line notebook app
+**Created with :heart: in ![#c5f015](⬢) [Node.js][] by [Jeremy Swanborough][]**
+
+## Requirements:
+* Git
+* [Node.js][]
+
+## Installation:
+* 1. Clone the repo and open the notebook dir
+```console
+λ git clone https://github.com/jeremyswann/notebook.git notebook
+cd notebook
+```
+* 2. Install the app using npm or yarn
+```console
+λ npm i
+```
+
+## How to use:
+Run `--help` to get a list of possible commands
+
+```console
+λ node app --help
+```
+
+Have fun! :tada:
+
+[Node.js]: https://nodejs.org/en/
+[Jeremy Swanborough]: https://github.com/jeremyswann
---
+++ app.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const _ = require('lodash')
+const yargs = require('yargs')
+
+const notes = require('./notes')
+
+const titleOptions = {
+	describe: 'Title of note',
+	demand: true,
+	alias: 't',
+}
+const argv = yargs
+	.command('add', 'Add a new note', {
+		title: titleOptions,
+		body: {
+			describe: 'Body of note',
+			demand: true,
+			alias: 'b',
+		},
+	})
+	.command('list', 'List all notes')
+	.command('read', 'Read a note', {
+		title: titleOptions,
+	})
+	.command('remove', 'Remove a note', {
+		title: titleOptions,
+	})
+	.version('1.0')
+	.help().argv
+
+const command = argv._[0]
+
+if (command === 'add') {
+	const note = notes.addNote(argv.title, argv.body)
+	const message = note
+		? notes.logNote('Note Created!', note)
+		: 'Note title taken'
+	console.log(message)
+	return note
+} else if (command === 'list') {
+	const allNotes = notes.getAll()
+	console.log(`Printing ${allNotes.length} note(s).`)
+	allNotes.forEach((note, i) => {
+		const message = notes.logNote(`\n[Note ${i + 1}]`, note)
+		console.log(message)
+	})
+} else if (command === 'read') {
+	const note = notes.getNote(argv.title)
+	const message = note ? notes.logNote('Note Found!', note) : `Note not found`
+	console.log(message)
+} else if (command === 'remove') {
+	const note = notes.removeNote(argv.title)
+	const message = note
+		? `Note Removed!\n--\nTitle: ${argv.title}`
+		: `Note title doesn't exist`
+	console.log(message)
+} else {
+	console.log('Command not recognised')
+}
